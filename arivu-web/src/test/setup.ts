ba9947014cff738d1f95e15ab952850f6973f4b1
rachit1994 <rachit.srivastava1994@gmail.com>
2026-03-21 03:34:37 +0530

    test: use node:crypto webcrypto in jsdom to fix SubtleCrypto importKey

--- arivu-web/src/test/setup.ts
+++ arivu-web/src/test/setup.ts
@@ -1,5 +1,15 @@
 import "@testing-library/jest-dom/vitest";
 
+import { webcrypto } from "node:crypto";
+
+// jsdom's SubtleCrypto can reject Node ArrayBuffers/TypedArrays in importKey (cross-realm).
+// Use Node's Web Crypto for tests so PKCS#8 buffers from kalshiAuth match crypto.subtle.
+Object.defineProperty(globalThis, "crypto", {
+  configurable: true,
+  value: webcrypto,
+  writable: true,
+});
+
 if (!("ResizeObserver" in globalThis)) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (globalThis as any).ResizeObserver = class {
